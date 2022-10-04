fac583ff360a0fa8d3307b6210f25258317e3ceb
gemtunde <gemtunde@gmail.com>
2022-10-04 11:20:03 +0100

    banner

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+      <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/hotels' element={<List />} />
+            <Route exact path='/hotel/:id' element={<Hotel />} />
+      </Routes>
+   </BrowserRouter>
   );
 }
 
