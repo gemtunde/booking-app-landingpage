2a54013cb5e6a7ed033e8032cd31a677b85d718b
gemtunde <gemtunde@gmail.com>
2022-10-06 03:20:04 +0100

    hotels list

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -20,10 +20,19 @@ const SearchItem = () => {
           <span className='siCancelOp'> Free cancellation</span>
           <span className='siCancelOpSubtitle'> 
             You can cancel later, so lock in this great price today!
-           </span>
-          <span className='siDistance'> 500m from center</span>
+           </span>         
         </div>
-        <div className="siDetails">see details</div>
+        <div className="siDetails">
+            <div className="siRating">
+                <span>Excellent</span>
+                <button>6.8</button>
+            </div>
+            <div className="siDetailTexts">
+                <span className="siPrice">$324</span>
+                <span className="siTaxOp">Includes taxes and fees</span>
+                <button className='siCheckButton'>See availability</button>
+            </div>
+        </div>
     </div>
   )
 }
